3e3bdaea478a0aed04160cf5bc1bcb9ec1032c26
Davdu <madsd@itu.dk>
2024-10-02 22:07:16 +0200

    Console Application for testing
    Co-Authored-By: Mikkel Kristensen <mbek@itu.dk>

--- TwitchLinkr/TwitchLinkr/EndpointCaller.cs
+++ TwitchLinkr/TwitchLinkr/EndpointCaller.cs
@@ -6,18 +6,9 @@ namespace TwitchLinkr
 	/// <summary>
 	/// Provides methods to call GET and POST endpoints with OAuth token and Client ID authentication.
 	/// </summary>
-	internal static class EndpointCaller
+	internal class EndpointCaller : IEndpointCaller
 	{
-		/// <summary>
-		/// Shapes the <c cref="HttpRequestException">HttpRequestException</c> to call a GET endpoint.
-		/// </summary>
-		/// <param name="endpoint"></param>
-		/// <param name="parameters"></param>
-		/// <param name="oauthToken"></param>
-		/// <param name="clientId"></param>
-		/// <param name="content"></param>
-		/// <exception cref="HttpRequestException"></exception>"
-		/// <returns><c>API</c> response as JSON.</returns>
+
 		public static async Task<string> CallGetEndpointAsync(string endpoint, Dictionary<string, string> parameters, string oauthToken, string clientId)
 		{
 			// Create a new HttpRequestMessage object with the endpoint as the URL.
@@ -38,16 +29,6 @@ namespace TwitchLinkr
 			return await CallAPIAsync(request);
 		}
 
-		/// <summary>
-		/// Shapes the <c cref="HttpRequestException">HttpRequestException</c> to call a POST endpoint.
-		/// </summary>
-		/// <param name="endpoint"></param>
-		/// <param name="parameters"></param>
-		/// <param name="oauthToken"></param>
-		/// <param name="clientId"></param>
-		/// <param name="content"></param>
-		/// <exception cref="HttpRequestException"></exception>"
-		/// <returns><c>API</c> response as JSON.</returns>
 		public static async Task<string> CallPostEndpointAsync(string endpoint, Dictionary<string, string> parameters, string oauthToken, string clientId, string content)
 		{
 			// Create a new HttpRequestMessage object with the endpoint as the URL.
@@ -72,11 +53,6 @@ namespace TwitchLinkr
 			return await CallAPIAsync(request);
 		}
 
-		/// <summary>
-		/// Calls the API as specified in the <paramref name="request"/>.
-		/// </summary>
-		/// <param name="request"></param>
-		/// <returns>API response as a string </returns>
 		public static async Task<string> CallAPIAsync(HttpRequestMessage request)
 		{
 			using (HttpClient client = new HttpClient())
---
+++ TwitchLinkr/TwitchLinkr/IEndpointCaller.cs
@@ -0,0 +1,43 @@
+﻿namespace TwitchLinkr
+{
+	/// <summary>
+	/// Provides methods to call GET and POST endpoints with OAuth token and Client ID authentication.
+	/// </summary>
+	internal interface IEndpointCaller
+    {
+
+		/// <summary>
+		/// Shapes the <c cref="HttpRequestException">HttpRequestException</c> to call a GET endpoint.
+		/// </summary>
+		/// <param name="endpoint"></param>
+		/// <param name="parameters"></param>
+		/// <param name="oauthToken"></param>
+		/// <param name="clientId"></param>
+		/// <param name="content"></param>
+		/// <exception cref="HttpRequestException"></exception>"
+		/// <returns><c>API</c> response as JSON.</returns>
+		abstract public static Task<string> CallGetEndpointAsync(string endpoint, Dictionary<string, string> parameters, string oauthToken, string clientId);
+
+		/// <summary>
+		/// Shapes the <c cref="HttpRequestException">HttpRequestException</c> to call a POST endpoint.
+		/// </summary>
+		/// <param name="endpoint"></param>
+		/// <param name="parameters"></param>
+		/// <param name="oauthToken"></param>
+		/// <param name="clientId"></param>
+		/// <param name="content"></param>
+		/// <exception cref="HttpRequestException"></exception>"
+		/// <returns><c>API</c> response as JSON.</returns>
+		abstract public static Task<string> CallPostEndpointAsync(string endpoint, Dictionary<string, string> parameters, string oauthToken, string clientId, string content);
+
+
+		/// <summary>
+		/// Calls the API as specified in the <paramref name="request"/>.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns>API response as a string </returns>
+		abstract public static Task<string> CallAPIAsync(HttpRequestMessage request);
+
+
+	}
+}
