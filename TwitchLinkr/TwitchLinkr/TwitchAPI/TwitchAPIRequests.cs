829bea638d20aac5de1a4b5feb706641e5178326
Davdu <madsd@itu.dk>
2024-10-06 22:19:52 +0200

    Update TwitchAPIRequests.cs

--- TwitchLinkr/TwitchLinkr/TwitchAPI/TwitchAPIRequests.cs
+++ TwitchLinkr/TwitchLinkr/TwitchAPI/TwitchAPIRequests.cs
@@ -3,5 +3,14 @@
 	internal class TwitchAPIRequests
 	{
 
+		//curl -X POST 'https://api.twitch.tv/helix/polls' \
+		//-H 'Authorization: Bearer vpx9etxs8bbii29krls1ljai1kdr' \
+		//-H 'Client-Id: dt4z41sa8uexdkjvt7uu9jjqm575' \
+		//-H 'Content-Type: application/json' \
+		//-d '{"broadcaster_id":"123456","title":"Streaming next Tuesday. Which time works best for you?","choices":[{"title":"9AM"},{"title":"10AM"},{"title":"7PM"},{"title":"8PM"},{"title":"9PM"}],"duration":300}'
+
+
+
+
 	}
 }
