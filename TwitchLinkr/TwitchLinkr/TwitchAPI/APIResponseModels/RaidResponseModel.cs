cfaf15cfbc7b327a1ff3159aa414291b87e0f290
Mads Dalgaard Andersen <madsd@itu.dk>
2024-10-11 13:53:47 +0200

    Added Raid Response model

---
+++ TwitchLinkr/TwitchLinkr/TwitchAPI/APIResponseModels/RaidResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLinkr.TwitchAPI.APIResponseModels
+{
+	internal class RaidResponseModel
+	{
+		public RaidResponseDataContent[] Data { get; set; } = default!;
+	}
+
+    internal class RaidResponseDataContent
+    {
+		string created_at { get; set; } = default!;
+		public bool is_mature { get; set; } = default!;
+		public DateTime CreatedAtDateTime => DateTime.Parse(created_at);
+    }
+}
