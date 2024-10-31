9f8cb0f3364cda3b4ceb5be79b573939f0ce1526
Mads Dalgaard Andersen <madsd@itu.dk>
2024-10-31 10:53:55 +0100

    Added Emote requests

---
+++ TwitchLinkr/TwitchLinkr/TwitchAPI/APIResponseModels/EmotesResponseModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLinkr.TwitchAPI.APIResponseModels
+{
+	internal class EmotesResponseModel
+	{
+
+		public Emote[] Data { get; set; } = default!;
+		public string Template { get; set; } = default!;
+	}
+
+	internal class Emote
+	{
+		public string Id { get; set; } = default!;
+		public string Name { get; set; } = default!;
+		public EmoteImages Images { get; set; } = default!;
+		public string Tier { get; set; } = default!;
+		public string EmoteType { get; set; } = default!;
+		public string EmoteSetId { get; set; } = default!;
+		public string[] Format { get; set; } = default!;
+		public string[] Scale { get; set; } = default!;
+		public string[] ThemeMode { get; set; } = default!;
+		
+	
+	
+	
+	}
+
+	internal class EmoteImages
+	{
+		public string Url_1x { get; set; } = default!;
+		public string Url_2x { get; set; } = default!;
+		public string Url_4x { get; set; } = default!;
+
+
+	}
+}
---
+++ TwitchLinkr/TwitchLinkr/TwitchAPI/Requests/EmoteRequests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TwitchLinkr.TwitchAPI.APIResponseModels;
+
+namespace TwitchLinkr.TwitchAPI.Requests
+{
+	/// <summary>
+	/// Provides methods to interact with Twitch emotes.
+	/// </summary>
+	internal class EmoteRequests
+	{
+		/// <summary>
+		/// Gets the list of channel emotes for a specified broadcaster.<br/>
+		/// Requires an App Access Token or User Access Token.
+		/// </summary>
+		/// <param name="oAuthToken">The OAuth token for authentication.</param>
+		/// <param name="clientId">The Client ID for authentication.</param>
+		/// <param name="broadcasterId">The ID of the broadcaster whose emotes are being fetched.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the emotes response model.</returns>
+		public static async Task<EmotesResponseModel> GetChannelEmotesAsync(string oAuthToken, string clientId, string broadcasterId)
+		{
+			const string endpoint = "https://api.twitch.tv/helix/chat/emotes";
+
+			var parameters = new List<KeyValuePair<string, string>>
+				{
+					new KeyValuePair<string, string>("broadcaster_id", broadcasterId)
+				};
+
+			var response = await EndpointCaller.CallGetEndpointAsync(endpoint, oAuthToken, clientId, parameters.ToArray());
+
+			var data = JsonSerializer.Deserialize<EmotesResponseModel>(response);
+
+			return data!;
+		}
+
+		/// <summary>
+		/// Gets the list of global emotes.<br/>
+		/// Requires an App Access Token or User Access Token.
+		/// </summary>
+		/// <param name="oAuthToken">The OAuth token for authentication.</param>
+		/// <param name="clientId">The Client ID for authentication.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the emotes response model.</returns>
+		public static async Task<EmotesResponseModel> GetGlobalEmotesAsync(string oAuthToken, string clientId)
+		{
+			const string endpoint = "https://api.twitch.tv/helix/chat/emotes/global";
+
+			var response = await EndpointCaller.CallGetEndpointAsync(endpoint, oAuthToken, clientId);
+
+			var data = JsonSerializer.Deserialize<EmotesResponseModel>(response);
+
+			return data!;
+		}
+
+		/// <summary>
+		/// Gets the list of emotes for specified emote sets.<br/>
+		/// Requires an App Access Token or User Access Token.
+		/// </summary>
+		/// <param name="oAuthToken">The OAuth token for authentication.</param>
+		/// <param name="clientId">The Client ID for authentication.</param>
+		/// <param name="EmoteSetIds">An array of emote set IDs to fetch emotes for.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the emotes response model.</returns>
+		public static async Task<EmotesResponseModel> GetEmoteSetsAsync(string oAuthToken, string clientId, params string[] EmoteSetIds)
+		{
+			const string endpoint = "https://api.twitch.tv/helix/chat/emotes/set";
+
+			var parameters = new List<KeyValuePair<string, string>>();
+
+			foreach (var setId in EmoteSetIds)
+			{
+				parameters.Add(new KeyValuePair<string, string>("emote_set_id", setId));
+			}
+
+			var response = await EndpointCaller.CallGetEndpointAsync(endpoint, oAuthToken, clientId, parameters.ToArray());
+
+			var data = JsonSerializer.Deserialize<EmotesResponseModel>(response);
+
+			return data!;
+		}
+	}
+}
