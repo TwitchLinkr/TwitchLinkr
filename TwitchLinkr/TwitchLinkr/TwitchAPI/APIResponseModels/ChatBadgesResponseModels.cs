e066974f7e673cc10dff0eacee1fb712ce7f6777
Mads Dalgaard Andersen <madsd@itu.dk>
2024-11-01 19:45:19 +0100

    Added Badges

---
+++ TwitchLinkr/TwitchLinkr/TwitchAPI/APIResponseModels/ChatBadgesResponseModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLinkr.TwitchAPI.APIResponseModels
+{
+	internal class ChatBadgesResponseModel
+	{
+
+		public Badge[] Data { get; set; } = default!;
+
+	}
+
+	internal class Badge
+	{
+
+		public string SetId { get; set; } = default!;
+		public BadgeVersions[] Versions { get; set; } = default!;
+
+	}
+
+	internal class BadgeVersions
+	{
+		public string Id { get; set; } = default!;
+		public string ImageUrl1x { get; set; } = default!;
+		public string ImageUrl2x { get; set; } = default!;
+		public string ImageUrl4x { get; set; } = default!;
+		public string Description { get; set; } = default!;
+		public string ClickAction { get; set; } = default!;
+		public string ClickUrl { get; set; } = default!;
+
+	}
+}
---
+++ TwitchLinkr/TwitchLinkr/TwitchAPI/Requests/BadgeRequests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using TwitchLinkr.TwitchAPI.APIResponseModels;
+
+namespace TwitchLinkr.TwitchAPI.Requests
+{
+	internal class BadgeRequests
+	{
+
+		public static async Task<ChatBadgesResponseModel> GetChannelChatBadgesAsync(string oAuthToken, string clientId, string broadcasterId)
+		{
+			const string endpoint = "https://api.twitch.tv/helix/chat/badges";
+
+			// Format the parameters
+			var parameters = new List<KeyValuePair<string, string>>
+			{
+				new KeyValuePair<string, string>("broadcaster_id", broadcasterId)
+			};
+
+			// Call the endpoint
+			var response = await EndpointCaller.CallGetEndpointAsync(endpoint, oAuthToken, clientId, parameters.ToArray());
+
+			var data = JsonSerializer.Deserialize<ChatBadgesResponseModel>(response);
+
+			return data!;
+
+		}
+
+		public static async Task<object> GetGlobalChatBadgesAsync(string oAuthToken, string clientId)
+		{
+			const string endpoint = "https://api.twitch.tv/helix/chat/badges/global";
+
+			// Call the endpoint
+			var response = await EndpointCaller.CallGetEndpointAsync(endpoint, oAuthToken, clientId);
+
+			var data = JsonSerializer.Deserialize<ChatBadgesResponseModel>(response);
+			return data!;
+		}
+
+	}
+}
